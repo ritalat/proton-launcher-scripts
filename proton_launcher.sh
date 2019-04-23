8a9aa3e95dd0ba0d8bedbfccabdc2f777e54b932
ritalat <ritalat@fastmail.com>
2019-04-24 00:03:17 +0300

    Merge the scripts

--- proton_launcher.sh
+++ proton_launcher.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
 # USER CONFIG
 #EXE_NAME="game.exe"
-STEAM_LIBRARY="/path/to/steamapps"
+STEAM_PATH="${HOME}/.steam"
+STEAM_LIBRARY="${STEAM_PATH}/steam/steamapps"
 PROTON_VERSION="Proton 3.16 Beta"
 
 
@@ -11,32 +11,65 @@ PROTON_VERSION="Proton 3.16 Beta"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"
 
-if [ $# -ne 0 ]; then
+if [ "$1" = "-exe_name" ]; then
+    shift
     EXE_NAME="$1"
+    shift
 fi
 
-if [ -z ${EXE_NAME} ]; then
-    echo "Uncomment and correct EXE_NAME or give it as an argument!"
+if [ -z "${EXE_NAME}" ]; then
+    echo "Uncomment and set EXE_NAME or give it as an argument!"
+    echo "Example: $ ./proton_launcher.sh -exe_name someprogram.exe"
     exit
 fi
 
-if ! [ -d proton ]; then
-    mkdir proton
+if [ "${EXE_NAME}" = "winecfg" ] || [ "${EXE_NAME}" = "regedit" ] || [ "${EXE_NAME}" = "cmd" ]; then
+    echo "You have to run winecfg and other built-in wine tools through the wine command!"
+    echo "Example: $ ./proton_launcher.sh -exe_name wine winecfg"
+    exit
 fi
 
-export STEAM_COMPAT_DATA_PATH="${DIR}"/proton
-#export DXVK_HUD=version,devinfo,fps
-#export PROTON_LOG=1
-#export PROTON_DUMP_DEBUG_COMMANDS=1
-#export PROTON_DEBUG_DIR=1
-#export PROTON_USE_WINED3D=1
-#export PROTON_NO_D3D11=1
-#export PROTON_NO_D3D10=1
-#export PROTON_NO_ESYNC=1
-#export PROTON_FORCE_LARGE_ADDRESS_AWARE=1
-#export PROTON_OLD_GL_STRING=1
+if [ -z "${STEAM_COMPAT_DATA_PATH}" ]; then
+    echo "No STEAM_COMPAT_DATA_PATH set. Using ${DIR}/proton"
+    export STEAM_COMPAT_DATA_PATH="${DIR}"/proton
+fi
 
-# Launch the game
-"${STEAM_LIBRARY}/common/${PROTON_VERSION}/proton" run "${EXE_NAME}" "$@"
+if ! [ -d "${STEAM_COMPAT_DATA_PATH}"/pfx ]; then
+    mkdir -p "${STEAM_COMPAT_DATA_PATH}"/pfx
+fi
+
+# Running wine, winecfg, regedit or winetricks
+if [ "${EXE_NAME}" = "wine" ] || [ "${EXE_NAME}" = "winetricks" ]; then
+
+    export WINEPREFIX="${STEAM_COMPAT_DATA_PATH}/pfx"
+    export WINE="${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/bin/wine64"
+    export WINESERVER="${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/bin/wineserver"
+    export WINEDLLPATH="${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/lib64/wine:${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/lib/wine"
+    export PATH="${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/bin/:${STEAM_PATH}/ubuntu12_32/steam-runtime/amd64/bin:${STEAM_PATH}/ubuntu12_32/steam-runtime/amd64/usr/bin:${PATH}"
+    export LD_LIBRARY_PATH="${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/lib64:${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/lib:${STEAM_PATH}/ubuntu12_32/steam-runtime/pinned_libs_32:${STEAM_PATH}/ubuntu12_32/steam-runtime/pinned_libs_64:/usr/lib/x86_64-linux-gnu/libfakeroot:/lib/i386-linux-gnu:/usr/lib/i386-linux-gnu:/usr/local/lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu:/lib:/usr/lib:/usr/lib/i386-linux-gnu/i686:/usr/lib/i386-linux-gnu/sse2:/usr/lib/i386-linux-gnu/i686/sse2:${STEAM_PATH}/ubuntu12_32/steam-runtime/i386/lib/i386-linux-gnu:${STEAM_PATH}/ubuntu12_32/steam-runtime/i386/lib:${STEAM_PATH}/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu:${STEAM_PATH}/ubuntu12_32/steam-runtime/i386/usr/lib:${STEAM_PATH}/ubuntu12_32/steam-runtime/amd64/lib/x86_64-linux-gnu:${STEAM_PATH}/ubuntu12_32/steam-runtime/amd64/lib:${STEAM_PATH}/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu:${STEAM_PATH}/ubuntu12_32/steam-runtime/amd64/usr/lib:${LD_LIBRARY_PATH}"
+
+    # Run the commands
+    echo "running \"${EXE_NAME}\" directly with arguments: $@"
+    "${EXE_NAME}" "$@"
+
+# Running a normal .exe
+else
+
+    #export DXVK_HUD=version,devinfo,fps
+    #export PROTON_LOG=1
+    #export PROTON_DUMP_DEBUG_COMMANDS=1
+    #export PROTON_DEBUG_DIR=1
+    #export PROTON_USE_WINED3D=1
+    #export PROTON_NO_D3D11=1
+    #export PROTON_NO_D3D10=1
+    #export PROTON_NO_ESYNC=1
+    #export PROTON_FORCE_LARGE_ADDRESS_AWARE=1
+    #export PROTON_OLD_GL_STRING=1
+    
+    # Launch the game
+    echo "running \"${EXE_NAME}\" through proton with arguments: $@"
+    "${STEAM_LIBRARY}/common/${PROTON_VERSION}/proton" run "${EXE_NAME}" "$@"
+    
+fi
 
 
---
+++ readme.txt
@@ -0,0 +1,31 @@
+This script allows you to use Proton from your Steam installation as a Wine
+substitute for any Windows program.
+
+proton_launcher.sh is intended to be used for a single program. By default
+it creates a separate wineprefix "proton/pfx" inside the directory where the
+script is placed. For a more general system-wide Wine replacement, check the
+"global" directory in this repo (soonTM).
+
+Installation
+------------
+Place proton_launcher.sh in the same directory where the program you wan to run
+is installed. Edit STEAM_PATH, STEAM_LIBRARY and PROTON_VERSION near the
+beginning of the script to reflect your system. Uncomment and edit EXE_NAME to
+the program you want to run.
+
+Running
+-------
+If you set EXE_NAME inside the script, simply execute the script from your file
+manager or the terminal.
+
+You can override EXE_NAME by giving "-exe_name someother.exe" as an argument
+for the script.
+
+    $ ./proton_launcher.sh -exe_name someother.exe
+
+This way you can also run winecfg or winetricks on the prefix.
+
+    $ ./proton_launcher.sh -exe_name wine winecfg
+    $ ./proton_launcher.sh -exe_name winetricks
+    
+Note how you have to run winecfg and other built-in wine commands with "wine".
