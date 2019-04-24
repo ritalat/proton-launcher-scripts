6465b4dfc4b5be2918862c786562096622163f6e
ritalat <ritalat@fastmail.com>
2019-04-24 19:11:14 +0300

    Set WINE to wine instead of wine64

--- proton_launcher.sh
+++ proton_launcher.sh
@@ -6,8 +6,18 @@ STEAM_PATH="${HOME}/.steam"
 STEAM_LIBRARY="${STEAM_PATH}/steam/steamapps"
 PROTON_VERSION="Proton 3.16 Beta"
 
+#export DXVK_HUD=version,devinfo,fps
+#export PROTON_LOG=1
+#export PROTON_DUMP_DEBUG_COMMANDS=1
+#export PROTON_DEBUG_DIR=1
+#export PROTON_USE_WINED3D=1
+#export PROTON_NO_D3D11=1
+#export PROTON_NO_D3D10=1
+#export PROTON_NO_ESYNC=1
+#export PROTON_FORCE_LARGE_ADDRESS_AWARE=1
+#export PROTON_OLD_GL_STRING=1
 
-# Initialization
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"
 
@@ -31,45 +41,27 @@ fi
 
 if [ -z "${STEAM_COMPAT_DATA_PATH}" ]; then
     echo "No STEAM_COMPAT_DATA_PATH set. Using ${DIR}/proton"
-    export STEAM_COMPAT_DATA_PATH="${DIR}"/proton
+    export STEAM_COMPAT_DATA_PATH="${DIR}/proton"
 fi
 
-if ! [ -d "${STEAM_COMPAT_DATA_PATH}"/pfx ]; then
-    mkdir -p "${STEAM_COMPAT_DATA_PATH}"/pfx
+if ! [ -d "${STEAM_COMPAT_DATA_PATH}/pfx" ]; then
+    mkdir -p "${STEAM_COMPAT_DATA_PATH}/pfx"
 fi
 
 export WINEPREFIX="${STEAM_COMPAT_DATA_PATH}/pfx"
-export WINE="${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/bin/wine64"
+export WINE="${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/bin/wine"
 export WINESERVER="${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/bin/wineserver"
 export WINEDLLPATH="${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/lib64/wine:${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/lib/wine"
 export PATH="${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/bin/:${STEAM_PATH}/ubuntu12_32/steam-runtime/amd64/bin:${STEAM_PATH}/ubuntu12_32/steam-runtime/amd64/usr/bin:${PATH}"
 export LD_LIBRARY_PATH="${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/lib64:${STEAM_LIBRARY}/common/${PROTON_VERSION}/dist/lib:${STEAM_PATH}/ubuntu12_32/steam-runtime/pinned_libs_32:${STEAM_PATH}/ubuntu12_32/steam-runtime/pinned_libs_64:/usr/lib/x86_64-linux-gnu/libfakeroot:/lib/i386-linux-gnu:/usr/lib/i386-linux-gnu:/usr/local/lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu:/lib:/usr/lib:/usr/lib/i386-linux-gnu/i686:/usr/lib/i386-linux-gnu/sse2:/usr/lib/i386-linux-gnu/i686/sse2:${STEAM_PATH}/ubuntu12_32/steam-runtime/i386/lib/i386-linux-gnu:${STEAM_PATH}/ubuntu12_32/steam-runtime/i386/lib:${STEAM_PATH}/ubuntu12_32/steam-runtime/i386/usr/lib/i386-linux-gnu:${STEAM_PATH}/ubuntu12_32/steam-runtime/i386/usr/lib:${STEAM_PATH}/ubuntu12_32/steam-runtime/amd64/lib/x86_64-linux-gnu:${STEAM_PATH}/ubuntu12_32/steam-runtime/amd64/lib:${STEAM_PATH}/ubuntu12_32/steam-runtime/amd64/usr/lib/x86_64-linux-gnu:${STEAM_PATH}/ubuntu12_32/steam-runtime/amd64/usr/lib:${LD_LIBRARY_PATH}"
 
-# Running wine, winecfg, regedit or winetricks
 if [ "${EXE_NAME}" = "wine" ] || [ "${EXE_NAME}" = "winetricks" ]; then
-
-    # Run the commands
     echo "running \"${EXE_NAME}\" directly with arguments: $@"
     "${EXE_NAME}" "$@"
 
-# Running a normal .exe
 else
-
-    #export DXVK_HUD=version,devinfo,fps
-    #export PROTON_LOG=1
-    #export PROTON_DUMP_DEBUG_COMMANDS=1
-    #export PROTON_DEBUG_DIR=1
-    #export PROTON_USE_WINED3D=1
-    #export PROTON_NO_D3D11=1
-    #export PROTON_NO_D3D10=1
-    #export PROTON_NO_ESYNC=1
-    #export PROTON_FORCE_LARGE_ADDRESS_AWARE=1
-    #export PROTON_OLD_GL_STRING=1
-    
-    # Launch the game
     echo "running \"${EXE_NAME}\" through proton with arguments: $@"
     "${STEAM_LIBRARY}/common/${PROTON_VERSION}/proton" run "${EXE_NAME}" "$@"
-    
 fi
 
 
